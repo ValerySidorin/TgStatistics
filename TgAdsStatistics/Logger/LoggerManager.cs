b253d6269ccbd91d7ab543a9d47ff2eb9dcb9790
Afsuga Arewreh <afsuga.arewreh@mail.ru>
2020-04-10 16:23:01 +0300

    upd

--- TgAdsStatistics/Logger/LoggerManager.cs
+++ TgAdsStatistics/Logger/LoggerManager.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,7 +12,7 @@ namespace TgAdsStatistics.Logger
 {
     public class LoggerManager
     {
-        private readonly ContextAccessor contextAccessor;
+        private IHttpContextAccessor httpContextAccessor;
         private readonly ApplicationContext db;
         private ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
         {
@@ -19,10 +20,10 @@ namespace TgAdsStatistics.Logger
         });
         private ILogger logger;
 
-        public LoggerManager(ApplicationContext db, ContextAccessor contextAccessor)
+        public LoggerManager(ApplicationContext db, IHttpContextAccessor httpContextAccessor)
         {
+            this.httpContextAccessor = httpContextAccessor;
             this.db = db;
-            this.contextAccessor = contextAccessor;
             loggerFactory.AddFile("logger.txt");
             logger = loggerFactory.CreateLogger("FileLogger");
         }
@@ -30,14 +31,14 @@ namespace TgAdsStatistics.Logger
         public void Log()
         {
             string requestbody;
-            using (Stream stream = contextAccessor.httpContextAccessor.HttpContext.Request.Body)
+            using (Stream stream = httpContextAccessor.HttpContext.Request.Body)
             {
                 using (StreamReader sr = new StreamReader(stream))
                 {
                     requestbody = sr.ReadToEnd();
                 }
             }
-            Logs log = new Logs { DateTime = DateTime.Now, Body = requestbody, Host = contextAccessor.httpContextAccessor.HttpContext.Request.Host.ToString(), Method = contextAccessor.httpContextAccessor.HttpContext.Request.Method, Path = contextAccessor.httpContextAccessor.HttpContext.Request.Path, Protocol = contextAccessor.httpContextAccessor.HttpContext.Request.Protocol };
+            Logs log = new Logs { DateTime = DateTime.Now, Body = requestbody, Host = httpContextAccessor.HttpContext.Request.Host.ToString(), Method = httpContextAccessor.HttpContext.Request.Method, Path = httpContextAccessor.HttpContext.Request.Path, Protocol = httpContextAccessor.HttpContext.Request.Protocol };
             db.Logs.Add(log);
             db.SaveChanges();
             logger.LogInformation($"{log.DateTime}, {log.Method}, {log.Path}, {log.Host}, {log.Protocol}, {log.Body}");
